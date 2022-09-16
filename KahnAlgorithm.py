3ff6cd9aa6af44c11ab69854338cde138947b82d
Jeffrey Tom <jct684@gmail.com>
2022-09-15 23:30:56 -0700

    minor changes

--- KahnAlgorithm.py
+++ KahnAlgorithm.py
@@ -1,4 +1,4 @@
-import queue
+from collections import deque
 
 class Graph():
 
@@ -22,23 +22,23 @@ class Graph():
 
 
 def Kahns_algorithm(graph):
-    q = queue.Queue()
+    q = deque()
     num_edges = {key:0 for key in graph}
     output = []
-    for k, v in graph.items():
+    for v in graph.values():
         for num in v:
             num_edges[num] += 1
     for k, v in num_edges.items():
         if v == 0:
-            q.put(k)
-    while q.qsize() > 0:
-        current = q.get()
+            q.append(k)
+    while len(q) > 0:
+        current = q.popleft()
         del num_edges[current]
         output.append(current)
         for nums in graph[current]:
             num_edges[nums] -= 1
             if num_edges[nums] == 0:
-                q.put(nums)
+                q.append(nums)
     return output
     
 
@@ -62,4 +62,5 @@ graph.insert_directed_edge(11,10)
 graph.insert_directed_edge(8,9)
 graph.show_graph()
 
+print("Expected: [5, 7, 3, 11, 8, 2, 10, 9]")
 print(Kahns_algorithm(graph.adjacency_list))
